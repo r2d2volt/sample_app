369e98809376458315bfcecf4ec891bf5f0172df
carlsed <carlsednaoui@gmail.com>
2011-05-27 21:37:11 -0400

    Done with static pages

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,17 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
-ENV["RAILS_ENV"] ||= 'test'
-require File.expand_path("../../config/environment", __FILE__)
+
+require 'rubygems'
+require 'spork'
+
+Spork.prefork do
+  #Loading more in this block will cause your tests to run faster. However, 
+  # if you need to change any configuration or code from libraries loaded here, you'll
+  # need to restart spork for it take effect.
+  ENV["RAILS_ENV"] ||= 'test'
+  unless defined? (Rails)
+    require File.dirname(__FILE__) + "/../config/environment"
+  end
+
 require 'rspec/rails'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
@@ -17,11 +28,19 @@ RSpec.configure do |config|
   # config.mock_with :rr
   config.mock_with :rspec
 
-  # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
   config.use_transactional_fixtures = true
+
+  ### Part of a Spork hack. See http://bit.ly/ary19y
+  # Emulate initializer set_clear_dependencies_hook in
+  #railties/lib/rails/application/bootstrap.rb
+  ActiveSupport::Dependencies.clear 
+end
+end
+
+Spork.each_run do
 end
